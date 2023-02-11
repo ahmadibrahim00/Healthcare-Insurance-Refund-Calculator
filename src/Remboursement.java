9d77e7f911a3a623fc9b7e8b374bd95271c93a6d
michaud.vincent.3 <michaud.vincent.3@courrier.uqam.ca>
2023-02-11 13:24:29 -0500

    Correction de l'acces aux attributs d'instance

--- src/Remboursement.java
+++ src/Remboursement.java
@@ -3,11 +3,23 @@ import java.util.ArrayList;
 public class Remboursement {
     private long client;
     private String dateReclamation;
-    public ArrayList<Soin> soinsRembourses;
+    private ArrayList<Soin> soinsRembourses;
 
     public Remboursement(long client, String dateReclamation, ArrayList<Soin> soinsRembourses) {
         this.client = client;
         this.dateReclamation = dateReclamation;
         this.soinsRembourses = soinsRembourses;
+    }
+
+    public long getClient() {
+        return client;
+    }
+
+    public String getDateReclamation() {
+        return dateReclamation;
+    }
+
+    public ArrayList<Soin> getSoinsRembourses() {
+        return soinsRembourses;
     }
 }
