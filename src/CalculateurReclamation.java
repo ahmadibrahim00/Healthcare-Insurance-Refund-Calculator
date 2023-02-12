68c3a1dd625381aa9419571360aa37b64995e394
michaud.vincent.3 <michaud.vincent.3@courrier.uqam.ca>
2023-02-12 04:56:04 -0500

    ajout d'une methode qui permet d'utiliser calculerRemboursement() en dehors des boucles for pour ne plus dupliquer l'accumulation des compteurs

--- src/CalculateurReclamation.java
+++ src/CalculateurReclamation.java
@@ -4,28 +4,61 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class CalculateurReclamation {
-    public CalculateurReclamation() {
+
+    static ArrayList<Double> montantsRembourses;
+
+
+    public CalculateurReclamation() throws IOException, ParseException {
+        this.montantsRembourses = ecrireSoinsRembourses();
     }
 
+    /**
+     * Genere les informations d'une reclamation a partir du fichier JSON
+     * @return Reclamation sous form d'ArrayList
+     * @throws IOException
+     * @throws ParseException
+     */
     private static ArrayList<Soin> genererReclamation() throws IOException, ParseException {
         JSONHash JSON = new JSONHash("Assurance.json", "resultat.json");
         JSON.load();
         ArrayList<Soin> reclamation = new ArrayList();
 
-        for(int i = 0; i < JSON.getNbSoin(); ++i) {
-            reclamation.add(new Soin(JSON.getNumClient(), JSON.getContrat(), JSON.getMois(), JSON.getSoin(i), JSON.getDate(i), (double)JSON.getMontant(i)));
+        for (int i = 0; i < JSON.getNbSoin(); ++i) {
+            reclamation.add(new Soin(JSON.getNumClient(), JSON.getContrat(), JSON.getMois(), JSON.getSoin(i), JSON.getDate(i), (double) JSON.getMontant(i)));
         }
-
         return reclamation;
     }
 
+    /**
+     * Appelle la methode qui calcule les remboursements et retourne les donnes dans un Arraylist
+     * @return Un ArrayList de double
+     * @throws IOException
+     * @throws ParseException
+     */
+    public static ArrayList<Double> ecrireSoinsRembourses() throws IOException, ParseException {
+        ArrayList<Double> soinsRembourses = new ArrayList<>();
+        for (int i = 0; i < genererReclamation().size(); ++i) {
+            soinsRembourses.add(genererReclamation().get(i).calculerRemboursement());
+        }
+        return soinsRembourses;
+    }
+
+    /**
+     * Permet d'obtenir les informations de tous les soins auxquels on a applique un remboursement sous forme d'ArrayList
+     * @return Un ArrayList des soins rembourses
+     * @throws IOException
+     * @throws ParseException
+     */
     public static ArrayList<Soin> getSoinsRembourses() throws IOException, ParseException {
         ArrayList<Soin> soinsRembourses = new ArrayList();
 
-        for(int i = 0; i < genererReclamation().size(); ++i) {
-            soinsRembourses.add(new Soin(((Soin)genererReclamation().get(i)).getClient(), ((Soin)genererReclamation().get(i)).getTypeContrat(), ((Soin)genererReclamation().get(i)).getDateReclamation(), ((Soin)genererReclamation().get(i)).getNumeroSoin(), ((Soin)genererReclamation().get(i)).getDateSoin(), ((Soin)genererReclamation().get(i)).calculerRemboursement()));
+        for (int i = 0; i < genererReclamation().size(); ++i) {
+            soinsRembourses.add(new Soin(((Soin) genererReclamation().get(i)).getClient(), ((Soin) genererReclamation().get(i)).getTypeContrat(),
+                    ((Soin) genererReclamation().get(i)).getDateReclamation(), ((Soin) genererReclamation().get(i)).getNumeroSoin(),
+                    ((Soin) genererReclamation().get(i)).getDateSoin(), montantsRembourses.get(i)));
         }
 
         return soinsRembourses;
     }
+
 }
