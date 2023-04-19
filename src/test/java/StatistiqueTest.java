956dc687f6fd15a352372056722142416a3416c3
Ahmad Ibrahim <ibrahim.ahmad@courrier.uqam.ca>
2023-04-18 22:51:42 -0400

    Ajout des méthodes de test pour StatistiqueTest

--- src/test/java/StatistiqueTest.java
+++ src/test/java/StatistiqueTest.java
@@ -9,12 +9,59 @@ import static org.junit.Assert.assertEquals;
 public class StatistiqueTest
 {
     Statistique stats = new Statistique();
+    JSONHash a = new JSONHash("Assurance.json", "Test.json");
 
     @Test
     public void testgetStatistiques() throws IOException, ParseException {
-        stats.modifierFichierStatistiques();
+        stats.formaterFichierStatistiques();
         stats.getStatistiques();
-        assertEquals(2, 2);
+        a.load();
+        stats.calculerStatistiques(a);
+        long reclamationsvalides = (long) stats.reclamationsValides;
+        assertEquals(3, reclamationsvalides);
     }
+    @Test
+    public void testgetnombrepartypedesoins() throws IOException, ParseException{
+        stats.formaterFichierStatistiques();
+        stats.getStatistiques();
+        a.load();
+        stats.calculerStatistiques(a);
+       int soin0 = (int) stats.soin0;
+       int soin200 = (int) stats.soin200;
+       int soin3xx = (int) stats.soin300;
+       assertEquals(1, soin200);
+       assertEquals(0, soin0);
+       assertEquals(1, soin3xx);
+    }
+    @Test
+    public void testgetStatistiquesExistantes() throws IOException, ParseException{
+        stats.formaterFichierStatistiques();
+        stats.getStatistiques();
+        a.load();
+        stats.calculerStatistiques(a);
+        long statistiquesinvalides = stats.reclamationsInvalides;
+        assertEquals(0, statistiquesinvalides);
+    }
+
+    @Test
+    public void testAdditionnersoinspartype1() throws IOException, ParseException{
+        stats.formaterFichierStatistiques();
+        stats.getStatistiques();
+        a.load();
+        stats.calculerStatistiques(a);
+        int soin100 = (int) stats.soin100;
+        assertEquals(1, soin100);
+    }
+
+    @Test
+    public void testAdditionnerSoinsParType2()throws IOException, ParseException{
+        stats.formaterFichierStatistiques();
+        stats.getStatistiques();
+        a.load();
+        stats.calculerStatistiques(a);
+        int soin300 = (int) stats.soin300;
+        assertEquals(1, soin300);
+    }
+
 
 }
